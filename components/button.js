b58eb503fe264c1caefc51efe7120468aaf30ea8
Enes Lisovac <lisovac.enes@gmail.com>
2017-11-27 20:47:11 +0100

    fifth commit

--- components/button.js
+++ components/button.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { TouchableHighlight, Text } from 'react-native';
 
 export default class Button extends Component {
+    
     render() {
         return (
             <TouchableHighlight style={this.props.buttonStyle}
